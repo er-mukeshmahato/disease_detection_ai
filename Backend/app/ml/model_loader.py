5285a26201826194481acd053b2f9d61bc1de4f5
Mukesh Kr Mahato <53220246+er-mukeshmahato@users.noreply.github.com>
2025-11-10 13:14:42 +0900

    fix

--- Backend/app/ml/model_loader.py
+++ Backend/app/ml/model_loader.py
@@ -1,5 +1,12 @@
+import os
 from tensorflow.keras.models import load_model as keras_load_model
 
-def load_model(model_path: str):
-    model = keras_load_model(model_path)
+def load_model():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # main.py folder
+    MODEL_PATH = os.path.join(BASE_DIR, "ml", "chest_disease_model.h5")
+
+    if not os.path.exists(MODEL_PATH):
+        raise FileNotFoundError(f"Model file not found at: {MODEL_PATH}")
+
+    model = keras_load_model(MODEL_PATH)
     return model
--- Backend/app/routes/predict_api.py
+++ Backend/app/routes/predict_api.py
@@ -1,20 +1,14 @@
 from fastapi import APIRouter, UploadFile, File
 from app.ml.model_loader import load_model
 from app.ml.predict import predict_disease
-import os
-from tensorflow.keras.models import load_model
-
-# Get the absolute path to the current file's directory
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 router = APIRouter()
 
-# Path to your trained model
-MODEL_PATH = os.path.join(BASE_DIR, "app", "ml", "chest_disease_model.h5")
+
 CLASS_NAMES = ['Covid', 'Normal', 'Pneumonia', 'Pneumothorax', 'Tuberculosis']
 
 # Load the model once at startup
-model = load_model(MODEL_PATH)
+model = load_model()
 
 @router.post("/upload/")
 async def predict_image(file: UploadFile = File(...)):
