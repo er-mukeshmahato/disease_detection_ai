0fae5ed6c5590c2eb6ef73997d06cbd35cc27340
Mukesh Kr Mahato <53220246+er-mukeshmahato@users.noreply.github.com>
2025-11-11 03:41:56 +0900

    fix deploy issue

--- disease-detection-ui/src/App.tsx
+++ disease-detection-ui/src/App.tsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import AboutSystem from "./components/AboutSystem";
 import HowItWorks from "./components/HowItWorks";
