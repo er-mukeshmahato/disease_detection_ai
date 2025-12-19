7e49c20a4e245657a4c08dcfd22f18e0e198ec3a
Mukesh Kr Mahato <53220246+er-mukeshmahato@users.noreply.github.com>
2025-12-19 11:32:34 +0900

    backend added

--- Backend/app/auth/schema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, EmailStr # type: ignore
-
-class UserCreate(BaseModel):
-    email: EmailStr
-    password: str
-
-class UserLogin(BaseModel):
-    email: EmailStr
-    password: str
